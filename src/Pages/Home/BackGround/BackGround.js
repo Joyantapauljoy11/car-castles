146385d94a10095aff028265dfa23e79136134f7
Joyanta paul <joyantapauljoy11@gmail.com>
2022-01-31 17:29:38 +0600

    4 update

--- src/Pages/Home/BackGround/BackGround.js
+++ src/Pages/Home/BackGround/BackGround.js
@@ -9,7 +9,7 @@ const BackGround = () => {
                 <div className="d-flex align-items-center justify-content-center" style={{ height: '60vh', width: '100%' }}>
                     <div className="w-100">
                         <div className="row align-items-center">
-                            <h1 >Car Store</h1>
+                            <h1> Car Castles </h1>
                             <div className="col-12 col-md-6 text-end">
                                 <h1 className='fs-bold'>Our Goal and mission in 2022</h1>
                                 <p>A car (or automobile) is a wheeled motor vehicle used for transportation. Most definitions of cars say that they run primarily on roads, seat one-to-eight people, have four wheels and mainly transport people rather than goods. the 20th century, and developed economies depend on them. The year 1886 is regarded as the birth year of the car when German inventor Karl Benz patented his Benz Patent-Motorwagen. Cars became widely available in the early 20th century. One of the first cars accessible to the masses was the 1908 Model T, an American car manufactured by the Ford Motor Company. Cars were rapidly adopted in the US, where they replaced animal-drawn carriages and carts. In Europe and other parts of the world, demand for automobiles did not increase until after Wor</p>
